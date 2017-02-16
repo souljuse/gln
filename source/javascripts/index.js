c1ff1ef7ca5e0e397c7e6b04e1386132e703929b
Tommaso Valentini <t.valentini@cantierecreativo.net>
2017-02-16 16:56:02 +0100

    added js for form submission

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -16,6 +16,25 @@ $(document).ready(function(){
   $('.main-nav__first-level__item--stick-left').css('top', top);
   $('.main-nav__first-level__item--stick-right').css('top', top);
 
+
+  $("#contacts-form").submit(function(e) {
+
+      var url = "https://formspree.io/t.valentini@cantierecreativo.net";
+
+      $.ajax({
+             type: "POST",
+             url: url,
+             data: $("#contacts-form").serialize(),
+             success: function(data)
+             {
+                 alert("Request successfully sent!");
+             }
+           });
+
+      e.preventDefault();
+  });
+
+
 });
 
 $('#main-nav-toggler').on('click', function(e) {
