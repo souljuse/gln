b13ab8423771ce21d650738edb2aa7ebca5b5936
Marco Zampetti <m.zampetti@cantierecreativo.net>
2017-06-05 12:45:22 +0200

    google ad integration

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -17,7 +17,6 @@ $(document).ready(function(){
     var windowHeight = $(window).height();
     var headerHeight = $('.header').height();
     var heroHeight = (windowHeight-96);
-    console.log(heroHeight);
     $('.hero__body').height(heroHeight);
   }
 
@@ -26,25 +25,70 @@ $(document).ready(function(){
   $('.main-nav__first-level__item--stick-right').css('top', top);
 
   //form submission with FormSpree
-    $("#contacts-form").submit(function(e) {
+  $("#contacts-form").submit(function(e) {
 
-        var url = "https://formspree.io/info@florenceluxuryvillas.com";
+      var url = "https://formspree.io/info@florenceluxuryvillas.com";
 
-        $.ajax({
-               type: "POST",
-               url: url,
-               data: $("#contacts-form").serialize(),
-               success: function(data)
-               {
-                   alert("Request sent.")
-               }
-             });
+      $.ajax({
+             type: "POST",
+             url: url,
+             data: $("#contacts-form").serialize(),
+             success: function(data)
+             {
+                alert("Request sent.");
+                goog_snippet_vars = function() {
+                  var w = window;
+                  w.google_conversion_id = 851423657;
+                  w.google_conversion_label = "t-ENCIu9z3EQqeP-lQM";
+                  w.google_remarketing_only = false;
+                }
+                // DO NOT CHANGE THE CODE BELOW.
+                goog_report_conversion = function(url) {
+                  goog_snippet_vars();
+                  window.google_conversion_format = "3";
+                  var opt = new Object();
+                  opt.onload_callback = function() {
+                  if (typeof(url) != 'undefined') {
+                    window.location = url;
+                  }
+                }
+                var conv_handler = window['google_trackConversion'];
+                if (typeof(conv_handler) == 'function') {
+                  conv_handler(opt);
+                }
+                }
+             }
+           });
 
-        e.preventDefault();
-    });
+      e.preventDefault();
+  });
 });
 
 $('#main-nav-toggler').on('click', function(e) {
   e.preventDefault();
   $(this).parent().toggleClass('is-active');
+});
+
+$('.js-cta').on('click', function(e) {
+  goog_snippet_vars = function() {
+    var w = window;
+    w.google_conversion_id = 851423657;
+    w.google_conversion_label = "LOWhCISoyXEQqeP-lQM";
+    w.google_remarketing_only = false;
+  }
+  // DO NOT CHANGE THE CODE BELOW.
+  goog_report_conversion = function(url) {
+    goog_snippet_vars();
+    window.google_conversion_format = "3";
+    var opt = new Object();
+    opt.onload_callback = function() {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  }
+  var conv_handler = window['google_trackConversion'];
+  if (typeof(conv_handler) == 'function') {
+    conv_handler(opt);
+  }
+}
 });
